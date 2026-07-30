2887aeedda9e30325855c3160e5861e6ada8716e
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-30 16:24:46 +0700

    feat: enhance swagger documentation for authentication routes

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -3,7 +3,74 @@ import { login, register } from "../controllers/auth.controller.js";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /auth/register:
+ *   post:
+ *     description: register
+ *     tags :
+ *        - Auth
+ *     requestBody:
+ *       description: register into system
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             required :
+ *              - name
+ *              - email
+ *              - password
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Register Success
+ */
+
 router.post("/register", register);
+
+/**
+ * @openapi
+ * /auth/login:
+ *   post:
+ *     description: login .
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       description : login into system
+ *       required : true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             required :
+ *              - email
+ *              - password
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/x-www/form-urlencoded:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       401:
+ *         description: Invalid email or password
+ */
+
 router.post("/login", login);
 
 export default router;
